ae256964f628231e8329a16f376cc70830d46786
lymlhhj123 <524964426@qq.com>
2016-09-18 16:59:16 +0800

    Update save.py

--- hspider/save.py
+++ hspider/save.py
@@ -15,20 +15,42 @@ def save_item_to_file():
 	pool = redis.ConnectionPool(host=my_settings.host, port=my_settings.port, db=0)
 	r = redis.StrictRedis(connection_pool=pool)
 	while 1:
-		if r.llen('item_queue') != 0:
-			print 'item to save!'
-			result = r.brpop('item_queue',0)
-			item = result[1]
-			item =json.loads(item)
-			item['movie_name'] = (''.join(item['movie_name'])).encode('utf-8')
-			item['movie_year'] = (''.join(item['movie_year'])).encode('utf-8')
-			item['movie_type'] = ('-'.join(item['movie_type'])).encode('utf-8')
-			item['movie_rate'] = (''.join(item['movie_rate'])).encode('utf-8')
-			item['url'] = item['url'].encode('utf-8')
-
-			with open(my_settings.item_filename, 'a') as f:
-				f.write('%s, %s, %s, %s, %s%s'%(item['movie_name'], item['movie_year'], item['movie_type'], item['movie_rate'],item['url'], os.linesep))
-
+		try:
+			if r.llen('item_queue') != 0:
+				print 'item to save!'
+				result = r.brpop('item_queue',0)
+				item = result[1]
+				item =json.loads(item)
+				item['movie_name'] = (''.join(item['movie_name'])).encode('utf-8')
+				item['movie_year'] = (''.join(item['movie_year'])).encode('utf-8')
+				item['movie_type'] = ('-'.join(item['movie_type'])).encode('utf-8')
+				item['movie_rate'] = (''.join(item['movie_rate'])).encode('utf-8')
+				item['url'] = item['url'].encode('utf-8')
+	
+				with open(my_settings.item_filename, 'a') as f:
+					f.write('%s, %s, %s, %s, %s%s'%(item['movie_name'], item['movie_year'], item['movie_type'], item['movie_rate'],item['url'], os.linesep))
+		except KeyboardInterrupt:#捕获键盘上的ctrl+c
+			print u'''
+			退出程序,请按0
+			暂停程序,请按任何非0字符
+			'''
+			num = raw_input('>')
+			if num == '0':
+				r.save
+				sys.exit(1)
+			else:
+				print u'''
+				程序已经处于暂停状态
+				重新启动，请按任何非0字符
+				退出程序，请按0
+				'''
+				count = raw_input(">")
+				if count == '0':
+					r.save
+					sys.exit(1)
+				else:
+					print u'程序重新开始运行'
+					continue
 
 class Save(object):
 	"""
