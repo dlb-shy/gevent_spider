f2d672a65fa259d68544e2dc0ded95f039e44044
lymlhhj123 <524964426@qq.com>
2016-10-18 09:22:40 +0800

    Update http_request.py

--- hspider/http_request.py
+++ hspider/http_request.py
@@ -1,82 +1,78 @@
 #-*-coding:utf-8-*-
 #!/usr/bin python
-from gevent import monkey;monkey.patch_all()
+from gevent import monkey; monkey.patch_all()
 import requests
+import urlparse
 import re
 import json
 import redis
 import logging
 import logging.config
 import random
-import urllib3
-from parsel import Selector
+import datetime
+import time
+import requests; requests.adapters.DEFAULT_RETRIES = 5 
 from chardet.universaldetector import UniversalDetector
 from cStringIO import StringIO
 import my_settings
+import save
+import myconnection
 
-logging.config.dictConfig(my_settings.my_logging_config)
-logger = logging.getLogger('HSpider')
-
-class Hu_httprequest(object):
-
+class HttpRequest(myconnection.RedisConnect):
+	logging.config.dictConfig(my_settings.my_logging_config)
 	def __init__(self):
-		
-		#创建redis服务器的连接池
-		self.pool = redis.ConnectionPool(host=my_settings.host, port=my_settings.port, db=0)
-		self.r = redis.StrictRedis(connection_pool=self.pool)
+		#redis连接
+		super(HttpRequest, self).__init__()
+		self.throttle = Throttle()
+		self.findcache = FindCache()
+		self.logger = logging.getLogger('HSpider')
 
-		#使用urllib3,创建http连接池,重用底层的tcp/ip链接
-		self.http = urllib3.PoolManager(maxsize=10, num_pools=5)
-
-		self.patten = re.compile(r'/subject/[0-9]+/$')
-
-	def get(self, url):
+	def get(self, url, base_url):
 		"""
 
 		用于请求静态的html链接,然后解析页面
+		参数:
+			url:待抓取的url, str或者unicode
+			base_url:待抓取url来源页面的url,主要用作构造headers中的referer
 		"""
-		if self.r.scard('ip_set') == 0:#如果redis集合中的ip_set集合为空，就使用本机ip
-			try:
-				response = self.http.request(method='GET',
-				 							url=url, 
-				 							headers={'user-agent':random.choice(my_settings.user_agent),
-													'accept-encoding':"gzip, deflate, sdch",
-													'connection':'keep-alive'},#使得response是启用了gzip压缩的, 
-											timeout=5, 
-											redirect=False, 
-											retries=False)
-				logger.info('successfully get url [%s]', url)
-			except urllib3.exceptions.HTTPError, e:#捕获请求失败的url以及相应的错误,并写入redis的request_failed_queue队列
-				reason = '%s,%s'%(url, e)
-				logger.error('failed to crawl url [%s],the reason is [%s]', url, e)
-				self.r.rpush('request_failed_queue', reason)
-			else:
-				page_coding = webpage_code(response)
-				sel = Selector(text=response.data.decode(page_coding), type='html')
-				if self.patten.search(url):#url的格式符合正则表达式，说明对应的response需要提取结构化数据
-						item = my_settings.my_item#用于提取结构化数据
-						#提取结构化数据
-						item['movie_name'] = sel.xpath('//h1/span[@property="v:itemreviewed"]/text()').extract()
-						item['movie_year'] = sel.xpath('//span[@property="v:initialReleaseDate"]/text()').extract()
-						item['movie_type'] = sel.xpath('//span[@property="v:genre"]/text()').extract()
-						item['movie_rate'] = sel.xpath('//strong[@class="ll rating_num"]/text()').extract()
-						item['url'] = url
-
-						result = json.dumps(item)
-						self.r.rpush('item_queue', result)
-
-				else:
-					#提取待抓取的未过滤的urls
-					urls = sel.xpath('//a/@href').re(r'/subject/[0-9]+/$|/tag/.*')
-					urls = list(set(urls))
-					result = json.dumps((url, urls))#这里需要传输url的原因，是因为网页中提取出来的可能是相对网址，在后续操作中需要将相对网址补充为绝对网址
-					self.r.rpush('unbloom_url_queue', result)
-								
+		#执行数据库的缓存查找，如果可以找到网页就直接返回
+		if self.findcache.find_cache(url):
+			self.logger.info('[%s] has been download', url)
 		else:
-			pass
-
-
-
+			#如果redis集合中的ip_set集合为空，就使用本机ip
+			proxies = self.r.srandmember('ip_set') if self.r.scard('ip_set') else None
+			#设置请求延迟
+			self.throttle.request_delay(url)	
+			try:
+				host = urlparse.urlparse(url).netloc
+				headers={'user-agent':random.choice(my_settings.user_agent),
+						'accept-encoding':"gzip",
+						'host':host,
+						'accept':'text/html',
+						'accept-charset':'utf-8',
+						'accept-language':'zh-cn'}
+				if base_url:
+					headers['referer'] = base_url
+				response = requests.get(url=url, 
+						 				headers=headers,
+										timeout=5, 
+										allow_redirects=False,
+										proxies=proxies,
+										verify=True)
+				self.logger.info('successfully download url [%s]', url)
+			#捕获请求失败的url以及相应的错误,并写入redis的request_failed_queue队列
+			except requests.exceptions.RequestException, e:
+				#reason = '%s,%s'%(url, e)
+				self.logger.error('failed to crawl url [%s],the reason is [%s]', url, e)
+				#self.r.rpush('request_failed_queue', url)
+			else:
+				response.encoding = webpage_code(response)
+				#在这里传输response.headers的时候，由于json的原因，将它转换为字符串表示
+				#在后续操作中如果需要操作response.headers的话，使用eval()表达式即可
+				content = json.dumps({'url': url, 'http_code': response.status_code, 
+				           'response_headers': unicode(response.headers), 'html': response.text})
+				self.r.rpush('html', content)
+												
 	def get_js_webpage(self):
 		"""
 
@@ -84,13 +80,54 @@ class Hu_httprequest(object):
 		"""
 		pass
 
-
 	def get_sign_in_page(self):
 		"""
 
 		抓取需要登录的页面
 		"""
 		pass
+		
+
+class Throttle(object):
+	"""
+	用于请求调优
+	self.domains中会记录每一个不同host最后一次访问的时间
+	在每次请求url的时候，就提取出这个url的host,然后用当前
+	时间减去self.domains中记录的时间，用产生的差值与self.time_delay
+	进行比较即可得出一个需要睡眠的时间。最后我们在乘以一个系数
+	就尽可能的可以保证每次请求的延迟都不一样
+	"""
+	def __init__(self, time_delay=my_settings.time_delay):
+		#用于记录每个域名最后的访问时间
+		self.domains = {}
+		self.time_delay = time_delay
+
+	def request_delay(self, url):
+		domain = urlparse.urlparse(url).netloc
+
+		#获取domain最后的请求时间
+		last_request_time = self.domains.get(domain)
+		if last_request_time and self.time_delay > 0:
+		    sleep_time = self.time_delay - (datetime.datetime.now() - last_request_time).seconds
+		    if sleep_time > 0:
+		    	time.sleep(sleep_time * random.uniform(0.5, 1.25))
+		#更新domain最后一次的访问时间
+		self.domains[domain] = datetime.datetime.now()
+
+
+class FindCache(myconnection.MySQLConnect):
+	"""
+	查找mysql数据库中的网页是否有缓存
+	"""
+	def __init__(self):
+		super(FindCache, self).__init__()
+
+	def find_cache(self, url):
+		self.cousor.execute('select html from douban where url=%s',(url.encode('utf-8'),))
+		self.connect.commit()
+		html = self.cousor.fetchone()
+		a = 1 if html else 0
+		return a
 
 
 def webpage_code(response):#解决网页编码问题
@@ -98,15 +135,15 @@ def webpage_code(response):#解决网页编码问题
 
 	解决网页的编码问题,获取网页的编码
 	"""
-	patten = re.compile(r'<.*charset=(.+).*>')
-	if patten.search(response.data).group(1):#使用正则表达式检测网页中是否存在charset
+	patten = re.compile(r'charset=(.+)[\"\']>')
+	if patten.search(response.content) is not None:#使用正则表达式检测网页中是否存在charset
 		
-		charset = patten.search(response.data).group(1)
+		charset = patten.search(response.content).group(1)
 		return charset.strip('\' or \"').lower()
 
 	else:#使用chardet去猜测编码
 		
-		my_stringio = StringIO(response.data)
+		my_stringio = StringIO(response.content)
 		my_detector = UniversalDetector()
 		for x in my_stringio:
 			my_detector.feed(x)
