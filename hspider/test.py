4ed541bd75be6de26429672c781aa009b0571bc8
lymlhhj123 <524964426@qq.com>
2016-11-04 23:05:52 +0800

    nothing change

--- hspider/test.py
+++ hspider/test.py
@@ -10,10 +10,11 @@ pid = os.fork()
 if pid == 0:
 	test = save.Html_Handle()
 	test.parse_and_save_html()
+
 else:
-	myspider = spider.HuSpider()
+	myspider = spider.HSpider()
 	
-	if not myspider.r.llen('url_queue'):#计算长度影响性能，需要改掉
+	if not myspider.redis.lindex('url_queue'):
 		#如果redis服务器的url_queue队列为空，那么就请求初始url，
 		#否则直接从url_queue队列中取出url开始请求
 		myspider.start_request()
